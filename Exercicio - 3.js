e8f703c88eb77c7857530d7381a5e2305a43899b
Soza <souzadanielreis087@gmail.com>
2025-02-26 22:29:11 -0300

    Aula 26 02 c/ Aline, Java

---
+++ Exercicio - 3.js
@@ -0,0 +1,11 @@
+var a = 5
+    console. log("pre incremento", ++a);
+        console. log("pre incremento", a++);
+            console. log(a);
+        console. log("pos incremento", ++a);
+    console. log("pre decremento", --a);
+
+    console. log("5"==5);
+       console. log("5"===5);
+            console. log (5!=10);
+                console. log("5"!=5);
---
+++ Exercicio - 5.js
@@ -0,0 +1,13 @@
+var idade1 = 16
+var idade2 = 17
+var idade3 = 18
+var idade4 = 69
+var idade5 = 70
+    if(idade1 >= 16/17){
+        console. log("Se quiser pó vota")
+    }
+
+    else{
+        console. log("Sai juvena!")
+    }
+ 
